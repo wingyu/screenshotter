59f44afc4953c7dbeacf25e62a2e7255e80d5c95
Vincent Wong <wingyu64@gmail.com>
2016-10-02 13:51:11 +1100

    Update instructor.ex

--- lib/screenshotter/instructor.ex
+++ lib/screenshotter/instructor.ex
@@ -2,6 +2,7 @@ defmodule Screenshotter.Instructor do
   @fetcher Application.get_env(:screenshotter, :fetcher)
   @cleaner Application.get_env(:screenshotter, :cleaner)
 
+  #Need to refractor this, the returned values from other methods don't feel clean/make sense
   def run(url, bucket, dir) do
     @fetcher.run(url)
     |> Screenshotter.Uploader.run(bucket, dir)
