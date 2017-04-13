6f855f56af0c853d942e376d099e552acf682972
Vincent Wong <wingyu64@gmail.com>
2017-04-13 18:07:44 +1000

    make into Hex Package

--- mix.exs
+++ mix.exs
@@ -2,13 +2,17 @@ defmodule Screenshotter.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :screenshotter,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     escript: escript_config,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :screenshotter,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      escript: escript_config,
+      start_permanent: Mix.env == :prod,
+      description: "A application that takes a screenshot of a website then uploads it into S3",
+      deps: deps(),
+      package: package
+    ]
   end
 
   # Configuration for the OTP application
@@ -40,5 +44,14 @@ defmodule Screenshotter.Mixfile do
 
   defp escript_config do
     [ main_module: Screenshotter.CLI ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs"],
+      maintainers: ["Vincent Wong"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/wingyu/screenshotter"}
+    ]
   end
 end
