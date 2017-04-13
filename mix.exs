0b35e4c634e64a80521cb01681fe34f982e875e9
Vincent Wong <wingyu64@gmail.com>
2017-04-13 18:00:24 +1000

    remove unnessecary OTP stuff

--- mix.exs
+++ mix.exs
@@ -16,7 +16,8 @@ defmodule Screenshotter.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :hound,:ex_aws, :hackney, :poison, :logger],
-     mod: {Screenshotter, []}]
+     #mod: {Screenshotter, []}
+    ]
   end
 
   # Dependencies can be Hex packages:
